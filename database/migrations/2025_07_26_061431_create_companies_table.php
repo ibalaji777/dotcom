8ac0e7589c2ea95ab2ad4372678ac09ccb3446d1
ibalaji777 <balajiofficialmailme@gmail.com>
2025-07-26 11:50:06 +0530

    Model & migrations

---
+++ database/migrations/2025_07_26_061431_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('domain_name')->unique(); // company-specific domain
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('gst_number')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('country')->default('India');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
